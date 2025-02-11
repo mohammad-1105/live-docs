49f1520c5e20cee4a83e0febb7daa0fd04b247ac
Mohammad Aman <mohammadamaan1105@gmail.com>
2025-02-11 12:09:54 +0545

    (feat): Add initial project setup with ESLint, Prettier, and Tailwind CSS configuration; create document page and add tiptap editor

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Suspense } from "react";
+import { Editor } from "./components/editor";
+
+export default function DocumentPage() {
+  return (
+    <div>
+      <Editor />
+     
+    </div>
+  );
+}
